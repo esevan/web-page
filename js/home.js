6731ec683f15fc38b33ec27e6d855985ce64f665
Eunsoo Park <esevan.park@gmail.com>
2017-08-25 01:16:25 +0900

    Change parallelax effect to z-index method
    Signed-off-by: Eunsoo Park <esevan.park@gmail.com>

--- js/home.js
+++ js/home.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
   var $indexSection = $('.index-section');
   var $indexTitle = $('.index-title');
+  var $contentsWrapper = $('.contents-wrapper');
   var screenWidth = $(window).width(), screenHeight = $(window).height();
   var scrollX = window.scrollX, scrollY = window.scrollY;
+  var indexFadedOut = false;
 
   function adjustIndexSection() {
     $indexSection.css('background-size', screenWidth);
     $indexSection.css('height', screenWidth * 720 / 1080);
+    $indexSection.css('width', screenWidth);
+    $contentsWrapper.css('margin-top', $indexSection.height());
   }
 
-  function fixIndexSection() {
-    var sectionHeight = $indexSection.height();
-
-    if (scrollY <= sectionHeight) {
-      $indexSection.css('background-position-y', scrollY);
-      if (scrollY <= sectionHeight - $indexTitle.height() - 10)
-        $indexTitle.css('top', scrollY+10);
+  function fadeOutTitle() {
+    if (indexFadedOut && scrollY < ($indexSection.height() - $indexTitle.height())) {
+      indexFadedOut = false;
+      $indexTitle.fadeIn('fast', function() { $indexTitle.show(); });
+    } else if (scrollY > $indexSection.height() - $indexTitle.height()){
+      indexFadedOut = true;
+      $indexTitle.fadeOut('fast', function() { $indexTitle.hide(); });
     }
   }
 
@@ -30,7 +34,7 @@ $(document).ready(function() {
     scrollX = window.scrollX;
     scrollY = window.scrollY;
 
-    fixIndexSection();
+    fadeOutTitle();
   }
 
   (function init() {
